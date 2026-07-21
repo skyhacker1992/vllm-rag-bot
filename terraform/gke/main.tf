87773a2eb8b565d5ceae9dc434d2f27dd242a2ce
Jimmy Bonant <skyhacker1992@gmail.com>
2026-07-21 19:08:21 -0400

    Change default node pool to GPU-optimized pool
    Replaced default node pool with a GPU-optimized node pool.

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -33,8 +33,8 @@ resource "google_container_cluster" "vllm_cluster" {
 }
 
 # Node pool optimized for VLLM (you can adjust machine type later)
-resource "google_container_node_pool" "default_pool" {
-  name       = "default-pool"
+resource "google_container_node_pool" "gpu_pool" {
+  name       = "gpu_pool"
   location   = var.region
   cluster    = google_container_cluster.vllm_cluster.name
   node_count = 1

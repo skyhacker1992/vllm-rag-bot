e103c2ae6cf45df24456ba75879df169ed937ee4
Jimmy Bonant <skyhacker1992@gmail.com>
2026-07-21 21:04:21 -0400

    Increase disk size from 20GB to 50GB

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -47,7 +47,7 @@ resource "google_container_node_pool" "default_pool" {
 
   node_config {
     machine_type = "n1-standard-4"
-    disk_size_gb = 20
+    disk_size_gb = 50
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
